aa911f84a10ff2f2a6807e55a491dd3d7324b523
ohPatPat <patr5009@elev.techcollege.dk>
2022-02-10 10:54:51 +0100

    hopefully done now

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ let urlsToCache = [
 
 self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open('mysite-static-v3').then(function (cache) {
+        caches.open('static').then(function (cache) {
             return cache.addAll([
 
                 index.html,
@@ -27,3 +27,11 @@ self.addEventListener('install', function (event) {
         }),
     );
 });
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+        return response || fetch(event.request);
+      }),
+    );
+  });
